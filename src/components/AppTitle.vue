d29ab44c33369856bd67ff04e2781e6466ddd9db
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-03 15:46:07 +0200

    add: project end and working

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    data() {
+        return {
+            title: "La mia prima app con vue"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{title}}</h2>
+</template>
+
+<style>
+
+</style>
