395559cad97e4a94e5a478cfd67a5864a836fded
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-03 16:12:41 +0200

    add: some other tries

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -2,7 +2,7 @@
 export default{
     data() {
         return {
-            title: "La mia prima app con vue"
+            title: "My banner title"
         }
     }
 }
@@ -10,13 +10,28 @@ export default{
 
 <template>
     <div class="banner">
+        <h2>{{ title }}</h2>
         <img src="../assets/solar-flare.en.webp" alt="">
 
     </div>
 </template>
 
 <style>
+.banner {
+    position: relative;
+}
+
 .banner img {
-    width: 50% ;
+    width: 100% ;
+}
+
+.banner h2{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+    background-color: rgba(0, 0, 0, 0.4);
+    color: white;
 }
 </style>
